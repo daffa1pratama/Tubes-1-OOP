49a1b15742e5c001c77e3c67ba4b4691718da896
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 18:43:42 +0700

    Update UnaryFinder.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expfinder/UnaryFinder.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expfinder/UnaryFinder.kt
@@ -2,14 +2,17 @@ package com.haverzard.smartcalculator.expfinder
 
 import com.haverzard.smartcalculator.expression.*
 
+// Kelas UnaryFinder : Turunan ExpressionFinder
 class UnaryFinder: ExpressionFinder {
     private var code : String
     private var x : Expression
+    
     constructor(_code: String, _x: Expression) {
         code = _code
         x = _x
     }
 
+    // Call() : mencari "kode" parsing yang sesuai
     override fun call() : Expression = when (code) {
         "u001" -> NegativeExpression(x)
         "u002" -> SinExpression(x)
