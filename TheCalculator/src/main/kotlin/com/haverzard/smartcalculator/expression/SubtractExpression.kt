82332cb84db2f1dcf021cdf57cfb5f2a0cb9a6a5
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 19:45:37 +0700

    Update SubtractExpression.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SubtractExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SubtractExpression.kt
@@ -1,6 +1,9 @@
 package com.haverzard.smartcalculator.expression
 
+// Kelas SubstractExpression : Turunan BinaryExpression
 class SubtractExpression(x : Expression,y : Expression) : BinaryExpression(x,y) {
+    
+    // Solve() : menghitung x - y
     override fun solve() : Double {
         return x - y
     }
