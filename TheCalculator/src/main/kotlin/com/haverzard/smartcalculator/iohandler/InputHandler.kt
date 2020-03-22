807489234811d6fd78625aef3f12236337d5f6be
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 20:01:59 +0700

    Update InputHandler.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/iohandler/InputHandler.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/iohandler/InputHandler.kt
@@ -2,27 +2,34 @@ package com.haverzard.smartcalculator.iohandler
 
 import java.util.*
 
+// Kelas InputHandler : Turunan IOHandler
 class InputHandler : IOHandler()  {
     private var inputDeque : Deque<String> = LinkedList<String>()
     private var bracketCounter = 0
+    
     fun enqueue(x : String) {
+    // Memasukkan x ke inputDeque
         inputDeque.addLast(x)
         val temp = lookup(x)
         if (openBracket(x)) bracketCounter++
         if (closeBracket(x)) bracketCounter--
     }
     fun dequeue(x: String) : String {
+    // Mengeluarkan x dari inputDeque
         return inputDeque.removeFirst()
     }
     fun replace(x : String) {
+    // Mengganti elemen terakhir dengan x
         inputDeque.removeLast()
         enqueue(x)
     }
     fun peeking() : String {
+    // Melihat elemen terakhir
         if (inputDeque.isNotEmpty()) return inputDeque.peekLast()
         return ""
     }
     fun getBracketCounts() : Int {
+    // Menghitung bracket (tanda kurung)
         return bracketCounter
     }
     fun stringInput() {
@@ -37,13 +44,16 @@ class InputHandler : IOHandler()  {
         }
     }
     operator fun contains(x : String) : Boolean {
+    // Mengecek apakah inputDeque mengandung x
         return inputDeque.contains(x)
     }
 
     override fun solve() {
+    // Mengevaluasi elemen inputDeque
         text = Solver(inputDeque).evaluate()
     }
     override fun clear() {
+    // Menghapus semua elemen inputDeque
         text = ""
         inputDeque.clear()
         bracketCounter = 0
