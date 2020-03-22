f341a6b9a093a328763af79e6f8332e758f9a198
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 19:15:10 +0700

    Update NegativeExpression.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/NegativeExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/NegativeExpression.kt
@@ -1,6 +1,9 @@
 package com.haverzard.smartcalculator.expression
 
+// Kelas Negative Expression : Turunan UnaryExpression
 class NegativeExpression(x: Expression) : UnaryExpression(x) {
+    
+    // Solve() : mengalikan x dengan -1 menjadi -x
     override fun solve() : Double {
         return - x
     }
