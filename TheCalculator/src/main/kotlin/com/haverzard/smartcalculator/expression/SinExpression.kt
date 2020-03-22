92fa69c64da45a7422ffbcc330dcd527da12f3db
Evan Pradanika <52844898+DufanGratis@users.noreply.github.com>
2020-03-22 16:07:30 +0700

    Update Trigonometri

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SinExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SinExpression.kt
@@ -7,7 +7,15 @@ class SinExpression(x : Expression) : UnaryExpression(x) {
         if ((x.solve() % 180.0) == 0.0) {
             return 0.0
         } else {
-            return sin(Math.toRadians(x.solve()))
+            if (((x.solve() % 30.0) == 0.0) && ((x.solve() % 60.0) != 0.0)) {
+                val rounded2 : Double
+                val rounded1 : Double
+                rounded2 = String.format("%.2f", sin(Math.toRadians(x.solve()))).toDouble()
+                rounded1 = String.format("%.1f", rounded2).toDouble()
+                return rounded1
+            } else {
+                return sin(Math.toRadians(x.solve()))
+            }
         }
     }
 }
