9da77919c41f188aefadf316dab14b43921b9526
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 19:44:31 +0700

    Update SquareExpression.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SquareExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/SquareExpression.kt
@@ -1,6 +1,9 @@
 package com.haverzard.smartcalculator.expression
 
+// Kelas SquareExpression : Turunan UnaryExpression
 class SquareExpression(x : Expression) : UnaryExpression(x) {
+    
+    // Solve() : menghitung x^2
     override fun solve() : Double {
         return x.solve()*x.solve()
     }
