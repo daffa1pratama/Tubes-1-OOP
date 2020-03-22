828c15b4148df3f336ad376f787637b01b6d8b76
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 19:18:32 +0700

    Update RootExpression.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/RootExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/RootExpression.kt
@@ -3,7 +3,10 @@ package com.haverzard.smartcalculator.expression
 import com.haverzard.smartcalculator.exception.ImaginaryException
 import kotlin.math.sqrt
 
+// Kelas RootExpression : Turunan UnaryExpression
 class RootExpression(x : Expression) : UnaryExpression(x) {
+    
+    // Solve() : operasi akar(x)
     override fun solve() : Double {
         val temp = sqrt(x.solve())
         if (x.solve() < 0) throw ImaginaryException(x.solve())
