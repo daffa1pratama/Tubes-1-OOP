287dec0f1b8b70fecb205b47d0385a277f25e6a7
DufanGratis <evan.hartoni@gmail.com>
2020-03-23 01:25:07 +0700

    Update IOHandlerTest

--- TheCalculator/src/test/kotlin/com/haverzard/smartcalculator/iohandler/IOHandlerTest.kt
+++ TheCalculator/src/test/kotlin/com/haverzard/smartcalculator/iohandler/IOHandlerTest.kt
@@ -2,6 +2,7 @@ package com.haverzard.smartcalculator.iohandler
 
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import com.haverzard.smartcalculator.iohandler.IOHandler
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import com.haverzard.smartcalculator.exception.*
@@ -14,9 +15,9 @@ import java.lang.Double.parseDouble
 import java.lang.NumberFormatException
 
 internal class IOHandlerTest {
-    var inputDeque = InputHandler()
     @Test
     fun inputHandlerTest() {
+        var inputDeque = InputHandler()
         var x = "27.0"
         var y = "0.53"
         var bracket1 = "ob"
@@ -35,11 +36,46 @@ internal class IOHandlerTest {
         inputDeque.enqueue(x)
         inputDeque.enqueue(bracket2)
         assertEquals(0, inputDeque.getBracketCounts())
+        inputDeque.stringInput()
+        assertEquals("( 27.0 ) ", inputDeque.text)
+        inputDeque.clear()
+        assertEquals("", inputDeque.text)
+        assertEquals(0, inputDeque.getBracketCounts())
     }
 
     @Test
     fun outputHandlerTest() {
-
+        var inputDeque = InputHandler()
+        var outputDeque = OutputHandler(inputDeque)
+        var x = "27.0"
+        var y = "0..3"
+        var z = "0.3"
+        outputDeque.replaceOutput(x)
+        assertEquals("27.0", outputDeque.text)
+        assertEquals("", outputDeque.iHandler.peeking())
+        outputDeque.dequeue()
+        assertEquals("27.0", outputDeque.iHandler.peeking())
+        outputDeque.clear()
+        assertEquals("0", outputDeque.text)
+        assertEquals("", outputDeque.iHandler.text)
+        outputDeque.replaceOutput(y)
+        assertEquals(false, outputDeque.error)
+        outputDeque.checkError()
+        assertEquals(true, outputDeque.error)
+        outputDeque.enqueue("ob")
+        assertEquals(1, outputDeque.iHandler.getBracketCounts())
+        outputDeque.enqueue("ob")
+        assertEquals(2, outputDeque.iHandler.getBracketCounts())
+        outputDeque.enqueue(z)
+        outputDeque.enqueue("cb")
+        assertEquals(1, outputDeque.iHandler.getBracketCounts())
+        assertEquals("( ( 0.3 ) ", outputDeque.iHandler.text)
+        assertEquals(false, outputDeque.solved)
+        outputDeque.solve()
+        assertEquals("( ( 0.3 ) ", outputDeque.iHandler.text)
+        outputDeque.enqueue("cb")
+        outputDeque.solve()
+        assertEquals("", outputDeque.iHandler.text)
     }
 
     @Test
