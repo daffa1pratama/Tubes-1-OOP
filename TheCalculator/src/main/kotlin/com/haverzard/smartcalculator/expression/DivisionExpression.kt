ff11d44ef4206a534ad97f764845c3bbd3294e3d
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 19:10:14 +0700

    Update DivisionExpression.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/DivisionExpression.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expression/DivisionExpression.kt
@@ -4,7 +4,10 @@ import com.haverzard.smartcalculator.exception.DivisionException
 import com.haverzard.smartcalculator.exception.ImaginaryException
 import kotlin.math.sqrt
 
+// Kelas DivisionExpression : Turunan BinaryExpression
 class DivisionExpression(x : Expression,y : Expression) : BinaryExpression(x,y) {
+    
+    // Solve() : menghitung pembagian x/y
     override fun solve() : Double {
         if (y.solve() == 0.0) throw DivisionException(y.solve())
         return x / y
