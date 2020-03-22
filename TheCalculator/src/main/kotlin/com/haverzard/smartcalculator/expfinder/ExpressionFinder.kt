2384f4d0a41cb76c8c5d6c79f834fe654420d4d5
Daffa Pratama <52825963+daffa1pratama@users.noreply.github.com>
2020-03-22 18:40:03 +0700

    Update ExpressionFinder.kt

--- TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expfinder/ExpressionFinder.kt
+++ TheCalculator/src/main/kotlin/com/haverzard/smartcalculator/expfinder/ExpressionFinder.kt
@@ -2,6 +2,8 @@ package com.haverzard.smartcalculator.expfinder
 
 import com.haverzard.smartcalculator.expression.Expression
 
+// ABC ExpressionFinder
+// Parent UnaryFinder & BinaryFinder
 abstract class ExpressionFinder {
     abstract fun call() : Expression
 }
